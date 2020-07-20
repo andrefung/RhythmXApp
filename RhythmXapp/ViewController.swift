e0ca570c28fe832da60cb805ca5031f9799caccf
Andre Fung <BoobiBabies@andres-mbp.home>
2020-07-20 16:33:42 +1000

    initial xcode project creation

---
+++ RhythmXapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RhythmXapp
+//
+//  Created by Andre Fung on 20/7/20.
+//  Copyright © 2020 Andre Fung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
