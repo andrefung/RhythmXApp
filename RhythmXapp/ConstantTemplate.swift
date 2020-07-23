22080ff4c11c32b313b79e283d3589a4a6939438
Andre Fung <BoobiBabies@andres-mbp.home>
2020-07-23 17:30:38 +1000

    modified gitignore to stop tracking Constants.swift, added ConstantsTemplate.swift instead

---
+++ RhythmXapp/ConstantTemplate.swift
@@ -0,0 +1,23 @@
+//
+//  ConstantTemplate.swift
+//  RhythmXapp
+//
+//  Created by Andre Fung on 23/7/20.
+//  Copyright © 2020 Andre Fung. All rights reserved.
+//
+/*
+ Add your own keys below and uncomment the code
+ */
+
+
+import Foundation
+
+/*
+struct Constants {
+ 
+ static var API_KEY = ""
+ static var PLAYLIST_ID = ""
+ static var API_URL = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
+ 
+ }
+ */
