88a1d3a57adac2bef4d53f6fea1726d4bece5047
Andre Fung <BoobiBabies@andres-mbp.home>
2020-07-23 18:58:58 +1000

    Added tableview, custom protocal in the model, displaying video titles in the ableview

--- RhythmXapp/ConstantTemplate.swift
+++ RhythmXapp/ConstantTemplate.swift
@@ -10,14 +10,15 @@
  */
 
 
+
 import Foundation
 
-/*
+
 struct Constants {
  
- static var API_KEY = ""
- static var PLAYLIST_ID = ""
- static var API_URL = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
- 
+    static var API_KEY = ""
+    static var PLAYLIST_ID = ""
+    static var API_URL = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
+    static var VIDEOCELL_ID = "VideoCell"
  }
- */
+ 
